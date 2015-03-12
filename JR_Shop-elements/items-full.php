3008936a4a1666cf91372ce7a5b24b8f17c87e89
Jon Richards <jonr.web@gmail.com>
2015-03-12 16:39:23 +0000

    another stab at item page

--- JR_Shop-elements/items-full.php
+++ JR_Shop-elements/items-full.php
@@ -16,18 +16,38 @@ if ($safeArr[ss]) {
 <article class="shop-full" >
   <div>
 
-    <header class="item-header">
-      <h1><?php echo $shop_item[name]; ?></h1><br>
-      <h2><?php echo $shop_item[price] ?></h2>
-    </header>
 
-    <div class="item-sidebar">
+    <div class="item-images<?php echo trim($shop_item[info].' '.$shop_item[icon]); ?>">
 
-      <h4><?php echo $shop_item[quantity] ?></h4>
-      <button>Ask us</button>
-      <button>Shopping List (nyi)</button>
-      <button>Buy Now (nyi)</button>
-      <?php echo $shop_item[brand] ?>
+      <img src="<?php echo img_resize($shop_item[imgFirst], 'tile') ?>">
+
+      <?php if (!$safeArr[ss]) : ?>
+      <ul class="item-gallery">
+        <?php foreach ($shop_item[imgAll] as $galleryImg) : ?>
+
+        <li><img src="<?php echo img_resize($galleryImg, 'thumb') ?>">
+        </li>
+        <?php endforeach ?>
+      </ul>
+      <?php endif ?>
+
+    </div>
+
+    <div class="item-focus" >
+      <header class="item-header">
+        <h1><?php echo $shop_item[name]; ?></h1><br>
+        <h2><?php echo $shop_item[price] ?></h2>
+      </header>
+
+      <?php echo $shop_item[desc] ?>
+      <?php echo $shop_item[condition] ?>
+
+
+      <?php if ($shop_item[icon]) : ?>
+        <h4><?php echo $shop_item[watt] ?></h4>
+      <?php endif ?>
+
+            <?php echo $shop_item[brand] ?>
       <?php echo $shop_item[model] ?>
       <ul class="item-dimensions">
         <li><?php echo $shop_item[hFull] ?></li>
@@ -38,30 +58,19 @@ if ($safeArr[ss]) {
 
     </div>
 
-    <div class="item-focus <?php echo trim($shop_item[info].' '.$shop_item[icon]); ?>" >
+    <div class="item-sidebar">
 
-      <img src="<?php echo img_resize($shop_item[imgFirst], 'tile') ?>" >
+      <button>Ask us</button>
+      <button>Shopping List (nyi)</button>
+      <button>Buy Now (nyi)</button>
 
-      <?php echo $shop_item[desc] ?>
-      <?php echo $shop_item[condition] ?>
-
-
-      <?php if ($shop_item[icon]) : ?>
-        <h4><?php echo $shop_item[watt] ?></h4>
-      <?php endif ?>
-
-
+      <ul class="feature-list" >
+        <li>One Feature</li>
+        <li>Two Feature</li>
+        <li>Three Feature</li>
+        <li>Four Feature</li>
+      </ul>
     </div>
-  <?php if (!$safeArr[ss]) : ?>
-    <div class="item-images" >
-
-      <?php foreach ($shop_item[imgAll] as $galleryImg) : ?>
-
-        <img src="<?php echo img_resize($galleryImg, 'thumb') ?>" >
-      <?php endforeach ?>
-
-    </div>
-    <?php endif ?>
 
   </div>
   <?php var_dump($shop_item) ?>
