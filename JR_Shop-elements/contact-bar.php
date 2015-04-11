715a1ac56f6d41eaf37d151e4d7c164f6ae54e1c
jon-r <jonr.web@gmail.com>
2015-04-11 14:59:15 +0100

    form-7 placeholder 1.0

--- JR_Shop-elements/contact-bar.php
+++ JR_Shop-elements/contact-bar.php
@@ -5,17 +5,18 @@
 <article class="contact-bar flex-container">
 
   <form class="form-contact flex-2">
-    <h2>Contact us about <?php echo $safeArr[pgName]; ?></h2>
-    <p>Contact us today</p>
-<!--    <label for="Name">Name</label>-->
-    <input type="text" name="name" placeholder="Name">
-<!--    <label for="email">Email Address</label>-->
-    <input type="email" name="email" placeholder="Email Address">
-<!--    <label for="phone">Phone Number</label>-->
-    <input type="number" name="phone" placeholder="Phone Number">
+    <h2>Contact us</h2><br>
+    <label for="Name">Name</label>
+    <input type="text" id="name">
+    <label for="email">Email Address</label>
+    <input type="email" id="email" >
+    <label for="phone">Phone Number</label>
+    <input type="number" id="phone" >
 <!--    <label for="subject">Subject</label>-->
-    <input type="text" name="subject" placeholder="Message Subject">
-    <textarea placeholder="message">blah</textarea>
+<!--    <input type="text" name="subject" placeholder="Message Subject">-->
+    <label for="message">Message</label>
+    <textarea id="message" >Item: <?php echo $safeArr[pgName]; ?></textarea>
+    <button class="btn-red"><h3>Send</h3></button>
   </form>
 
   <div class="testimonial-slides flex-2">
