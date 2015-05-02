a0fb522ddc5078b8bfee8cac4d0448ae16fe4073
jon-r <jonr.web@gmail.com>
2015-05-02 15:19:42 +0100

    footer start

--- footer.php
+++ footer.php
@@ -1,28 +1,6 @@
     <footer class="primary-footer" role="contentinfo">
 
-      <nav class=" container">
-
-          <?php wp_nav_menu(array(
-            'container' => '',                              // remove nav container
-            'menu' => __( 'Header Links', 'bonestheme' ),   // nav name
-            'menu_class' => 'menu-header',                             // adding custom nav class
-            'theme_location' => 'header-links',             // where it's located in the theme
-            'fallback_cb' => ''                             // fallback function
-          )); ?>
-
-          <?php wp_nav_menu(array(
-            'container' => '',                              // remove nav container
-            'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
-            'menu_class' => 'menu-footer',                  // adding custom nav class
-            'theme_location' => 'footer-links',             // where it's located in the theme
-            'fallback_cb' => ''                             // fallback function
-          )); ?>
-
-        <p class="copyright">&copy;
-          <?php echo date( 'Y'); ?>
-          <?php bloginfo( 'name' ); ?>
-        </p>
-      </nav>
+    <?php include( "JR_Shop-elements/footer-bar.php"); ?>
 
     </footer>
 
