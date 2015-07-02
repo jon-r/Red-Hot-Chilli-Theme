e4de334a78cad8b2989a61c4e21a379fa0a17aa3
jon-r <jonr.web@gmail.com>
2015-07-02 16:54:33 +0100

    feedback fix - re-arrange top nav

--- includes/nav-bar.php
+++ includes/nav-bar.php
@@ -1,20 +1,5 @@
-<?php
-$headerType = is_front_page() ? 'home' : 'not-home';
-?>
-
-
-
-<nav class="flex-container primary-nav">
-
-<label class="menu-btn btn-red" for="menu-toggle">
-  <h2 class="text-icon-left menu-w">Shop</h2>
-</label>
-
-<input type="checkbox"  id="menu-toggle">
-
-  <menu class="nav-menu <?php echo $headerType ?>" >
-    <?php echo do_shortcode("[jr-shop id='shop-menu' cached=false]"); ?>
-  </menu>
+<nav class="primary-nav">
+  <?php echo do_shortcode("[jr-shop id='shop-menu' cached=true]"); ?>
 
   <?php if (is_front_page()) {
           echo do_shortcode("[jr-shop id='index-carousel' cached=true]");
@@ -22,7 +7,6 @@ $headerType = is_front_page() ? 'home' : 'not-home';
           echo do_shortcode("[jr-shop id='nav-breadcrumbs']");
         }
     ?>
-
 </nav>
 
 
