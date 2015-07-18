9b8ed20896b7ac35402bd3ebd23a4926912b21b7
jon-r <jonr.web@gmail.com>
2015-07-18 17:32:03 +0100

    finished new form design

--- footer.php
+++ footer.php
@@ -6,12 +6,6 @@
   <?php // all js scripts are loaded in library/bones.php ?>
   <?php wp_footer(); ?>
 
-  <?php // form validation included only where needed ?>
-  <?php if (isset($GLOBALS['hasMailForm']) ) {
-  echo '<script type="text/javascript" src="'
-    .get_template_directory_uri().'/library/js/validate.min.js" ></script>' ;
-  }?>
-
   </body>
 
 </html>
