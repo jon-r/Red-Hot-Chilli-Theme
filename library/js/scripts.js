a770b51482ae95cadac407f06497a8e1af946082
jon-r <jonr.web@gmail.com>
2015-08-20 14:07:56 +0100

    combined several similar css classes

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -270,7 +270,7 @@ $imgGalleryNext.click(function() {
 /* modal popups ---------------------------------------------------------------------- */
 // eg zoom and enhance
 var $imgGalleryModal  = $('#js-gallery-modal'),
-    $imgGalleryOpen   = $imgGalleryMain.find('.tile-hover.zoom'),
+    $imgGalleryOpen   = $('#js-gallery-zoom'),
     $imgGalleryClose  = $imgGalleryModal.find('.modal-close');
     $buyModal         = $('#js-buy-modal'),
     $buyModalOpen     = $('#js-buy-btn'),
