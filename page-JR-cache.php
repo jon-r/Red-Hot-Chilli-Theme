ef655b5636c7565e17e4b1bb970d3b8940f1757d
jon-r <jonr.web@gmail.com>
2015-09-25 15:12:49 +0100

    sprite location

---
+++ page-JR-cache.php
@@ -0,0 +1,10 @@
+<?php
+/* Template Name: Cache Pseudo Page
+*
+* Just A Blank page to call the cache function without any html
+*
+* THIS IS NOT A NORMAL TEMPLATE
+*/
+
+echo do_shortcode( "[jr-shop id='cache-results']");
+?>
